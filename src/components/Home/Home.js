7e6847601dcc332024db88ba7b4856eed1807c7a
Hossen <mdmitho199911@gmail.com>
2022-03-31 21:10:22 +0600

    API data load and display

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import Cart from '../Cart/Cart'
+import useTShirts from '../Hooks/useTShirts';
+import TShirt from '../TShirt/TShirt';
+import './Home.css'
 
 const Home = () => {
+    const [tshirts,setTShirts] = useTShirts()
     return (
-        <div>
-            <h1>This is Home</h1>
+        <div className='home-container'>
+            <div className="tshirt-container">
+             {
+                 tshirts.map(tShirt => <TShirt
+                 key={tShirt._id}
+                 tShirt={tShirt}
+
+                 ></TShirt>)
+             }
+            </div>
+            <div className="cart-container">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
